1044ce86ebc8cc12b644a13fe94f2cf9030a57d2
Abhikrishna Ravi <82082729+AbhikrishnaRavi@users.noreply.github.com>
2021-06-19 20:18:12 +0530

    Delete query3.sql

--- query3.sql
+++
@@ -1,9 +0,0 @@
-SELECT books.title FROM books_subjects 
-
-INNER JOIN subjects
-ON books_subjects.subject = subjects.id
-
-INNER JOIN books
-ON books_subjects.book = books.id
-
-WHERE subjects.name IN ("Technology","Politics");
