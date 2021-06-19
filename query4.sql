7b39e697c84b9491853e8b09b548201f1256cc7b
Abhikrishna Ravi <82082729+AbhikrishnaRavi@users.noreply.github.com>
2021-06-19 20:18:04 +0530

    Delete query4.sql

--- query4.sql
+++
@@ -1,1 +0,0 @@
-SELECT subjects.name FROM subjects INNER JOIN books_subjects ON subjects.id = books_subjects.subject INNER JOIN books ON books.id = books_subjects.book AND books.title = "Atomic Habits";
